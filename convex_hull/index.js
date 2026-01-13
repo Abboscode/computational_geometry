3b390fe1909686251be870e17b332fd046268610
Abbosjon Karshiev <s284350@studenti.polito.it>
2026-01-13 11:06:04 +0100

    feature: boundires as circle has been added

--- convex_hull/index.js
+++ convex_hull/index.js
@@ -1,9 +1,12 @@
 import { Nail } from "./points_and_lines.js"; 
-
+const canvas = document.getElementById("canvas");
 const NAIL_COUNT = 100;
-
+const center={x:400,y:400};
+const radius=200;
+const BOUND_X=Math.cos(Math.PI)+center.x;
+const BOUND_Y=Math.sin(Math.PI)+center.y;
 function draw() {
-  const canvas = document.getElementById("canvas");
+  
   if (!canvas) return; // Safety check
   const ctx = canvas.getContext("2d");
 
@@ -14,15 +17,17 @@ function draw() {
    
     const x = Math.random() * canvas.width/2;
     const y = Math.random() * canvas.height/2;
-    nails.push(new Nail(x, y));
+    const distanceForomCenter = x*x + y*y;
+    if(distanceForomCenter**0.5<radius){
+    nails.push(new Nail(x, y));}
   }
 
    ctx.fillStyle = "black";
   ctx.strokeStyle = "red";
 
   nails.forEach((nail) => {
-    ctx.strokeRect(nail.x+100, nail.y+100, 9, 9);
-    ctx.fillRect(nail.x+100, nail.y+100, 9, 9);
+    ctx.strokeRect(nail.x+100, nail.y+400, 9, 9);
+    ctx.fillRect(nail.x+100, nail.y+400, 9, 9);
   });
 }
 
